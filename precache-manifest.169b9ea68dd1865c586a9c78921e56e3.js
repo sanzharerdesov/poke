20403445ac0870fd58ef6a5f3946ae6deb7a1321
sanzharerdesov <sanzharerdesov@users.noreply.github.com>
2020-03-14 17:01:17 +0000

    deployed via Github Pages Deploy Action 🚀 for 2a0ce39634bf81a4a48fe291d2657c889342cefc

---
+++ precache-manifest.169b9ea68dd1865c586a9c78921e56e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934cefde3a86b5d6a5793b4382fde3a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6548030477b8b2e98e7",
+    "url": "/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "9231bb2b73e8803d7e31",
+    "url": "/static/css/main.720c73cd.chunk.css"
+  },
+  {
+    "revision": "d6548030477b8b2e98e7",
+    "url": "/static/js/2.6c21658c.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/static/js/2.6c21658c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9231bb2b73e8803d7e31",
+    "url": "/static/js/main.78b0b386.chunk.js"
+  },
+  {
+    "revision": "cbade69dc0eb2bbcb7b3",
+    "url": "/static/js/runtime-main.cc27bfd2.js"
+  }
+]);
