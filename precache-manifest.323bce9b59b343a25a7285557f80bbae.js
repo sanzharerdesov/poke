f4df5d69912210520c619eacd02f5677d0057f23
sanzharerdesov <sanzharerdesov@users.noreply.github.com>
2020-03-15 07:20:17 +0000

    deployed via Github Pages Deploy Action 🚀 for ae266b84c3624e26b9dc9502c739c77165637337

---
+++ precache-manifest.323bce9b59b343a25a7285557f80bbae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da8900da40dd8d48c832773d2c4ce92",
+    "url": "/poke/index.html"
+  },
+  {
+    "revision": "a5293364e9d8578785b6",
+    "url": "/poke/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "a41920f126d20f6200ea",
+    "url": "/poke/static/css/main.e0f5db00.chunk.css"
+  },
+  {
+    "revision": "a5293364e9d8578785b6",
+    "url": "/poke/static/js/2.5a2fe14d.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/poke/static/js/2.5a2fe14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41920f126d20f6200ea",
+    "url": "/poke/static/js/main.e2a6dda9.chunk.js"
+  },
+  {
+    "revision": "a3a1bc89e8bdce2790c6",
+    "url": "/poke/static/js/runtime-main.00cf5ba8.js"
+  }
+]);
